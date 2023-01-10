64cb7a86feef81c21bf8d4ee2703ad404182b75f
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-01-10 17:49:15 +0300

    Added empty base window and loaded it into qml engine

---
+++ Window.qml
@@ -0,0 +1,8 @@
+import QtQuick 2.15
+
+Window {
+    width: 1200
+    height: 600
+    visible: true
+    title: qsTr("MD3Desk")
+}
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import sys
+
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
+
+
 def main():
-    pass
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+
+    start_location_filename = "Window.qml"
+    engine.load(start_location_filename)
+
+    app.exec()
 
 
 if __name__ == '__main__':
