1636bf0daee7f3b4b6ec1aed7a1ba63c5f373118
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-01-18 15:08:14 +0300

    Created main logic file to bind devices

---
+++ logic/Logic.py
@@ -0,0 +1,20 @@
+from PySide6.QtQml import QQmlApplicationEngine
+
+from logic.L807.L807 import L807
+
+
+class Logic:
+    """Class to contain working modes and devices, to register it to QML components"""
+    def __init__(self, engine: QQmlApplicationEngine):
+        """
+        Inits a station logic
+        :param engine: needs to register some other logics / devices for gui layouts
+        """
+        self._engine = engine
+
+        self._l807 = L807()
+
+        self._register_logics()
+
+    def _register_logics(self):
+        self._engine.rootContext().setContextProperty("l807", self._l807)
