86830da2f514f7486f0fc3bd578cf8bd229e0b8d
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-02-01 10:15:52 +0300

    Registered dmd uzozm logic

--- logic/Logic.py
+++ logic/Logic.py
@@ -1,6 +1,7 @@
 from PySide6.QtQml import QQmlApplicationEngine
 
 from logic.AGL import AGL
+from logic.DMDUZOZM import DMDUZOZM
 from logic.KLU import KLU
 from logic.L807 import L807
 from logic.TLF1 import TLF1
@@ -19,6 +20,7 @@ class Logic:
         self._agl = AGL()
         self._tlf1 = TLF1()
         self._klu = KLU()
+        self._dmd_uzozm = DMDUZOZM()
 
         self._register_logics()
 
@@ -27,3 +29,4 @@ class Logic:
         self._engine.rootContext().setContextProperty("agl", self._agl)
         self._engine.rootContext().setContextProperty("tlf1", self._tlf1)
         self._engine.rootContext().setContextProperty("klu", self._klu)
+        self._engine.rootContext().setContextProperty("dmd_uzozm", self._dmd_uzozm)
