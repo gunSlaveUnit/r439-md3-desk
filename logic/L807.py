c889c29dc3e2d2ce2c86dd3fccd259493dd48e13
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-09 13:56:39 +0300

    Less code in L807 logic

--- logic/L807.py
+++ logic/L807.py
@@ -1,34 +1,17 @@
-from enum import Enum
-
 from PySide6.QtCore import Signal, Property, QObject
 
 
 class L807(QObject):
-    class Joint(int, Enum):
-        SWITCHED_OFF = 0
-
-    JOINT_MODES = ["Выключен"]
-
-    class SignalSource(int, Enum):
-        AG_L = 0
-
-    SIGNAL_SOURCE_MODES = ["АГ-Л"]
+    joint_changed = Signal()
+    signal_source_changed = Signal()
 
     def __init__(self):
         super().__init__()
 
-        self._joint = self.Joint.SWITCHED_OFF
-        self._signal_source = self.SignalSource.AG_L
+        self._joint = 0
+        self._signal_source = 0
 
     # region Joint
-
-    @Property(list, constant=True)
-    def joint_modes(self):
-        return self.JOINT_MODES
-
-    @Signal
-    def joint_changed(self):
-        pass
 
     @Property(int, notify=joint_changed)
     def joint(self):
@@ -44,14 +27,6 @@ class L807(QObject):
     # endregion
 
     # region SignalSource
-
-    @Property(list, constant=True)
-    def signal_source_modes(self):
-        return self.SIGNAL_SOURCE_MODES
-
-    @Signal
-    def signal_source_changed(self):
-        pass
 
     @Property(int, notify=signal_source_changed)
     def signal_source(self):
