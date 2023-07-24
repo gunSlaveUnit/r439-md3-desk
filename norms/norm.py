3fe5066b08c3fae2fffb85998d56b1127e91efe5
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-07-24 10:37:09 +0300

    Norm class has default check components method implementation

--- norms/norm.py
+++ norms/norm.py
@@ -1,3 +1,11 @@
+from typing import List
+
+from norms.component import NormComponent
+
+
 class Norm:
+    def __init__(self, components: List[NormComponent]):
+        self._components = components
+
     def check(self):
-        raise NotImplementedError()
+        return all([component.check() for component in self._components])
