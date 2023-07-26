9858745d0cfba4631c8dbde4ac876e80a393f65f
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-07-26 16:54:48 +0300

    Added server urls: authorization, user info url

--- station/src/settings.py
+++ station/src/settings.py
@@ -3,3 +3,12 @@ from pathlib import Path
 ROOT_PATH = Path(__file__).parent.parent
 SRC_PATH = ROOT_PATH / 'src'
 GUI_LAYOUTS_PATH = SRC_PATH / 'gui'
+
+API_VERSION = 1
+
+SERVER_URL = f"http://127.0.0.1:8000/api/v{API_VERSION}/"
+AUTH_URL = SERVER_URL + "auth/"
+REGISTER_URL = AUTH_URL + "sign-up/"
+LOGIN_URL = AUTH_URL + "sign-in/"
+LOGOUT_URL = AUTH_URL + "sign-out/"
+ME_URL = AUTH_URL + "me/"
