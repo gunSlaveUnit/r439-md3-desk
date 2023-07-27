bc6216aca6478fca117199e0e72fdb3daabda773
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-07-27 16:57:47 +0300

    Norm from QObject to register norms in qml engine

--- station/src/norms/norm.py
+++ station/src/norms/norm.py
@@ -1,10 +1,13 @@
 from typing import List
 
+from PySide6.QtCore import QObject
+
 from norms.component import NormComponent
 
 
-class Norm:
+class Norm(QObject):
     def __init__(self, components: List[NormComponent]):
+        super().__init__()
         self._components = components
 
     def check(self):
