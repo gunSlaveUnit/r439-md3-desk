d08a13126acbf1dcc6d3c089357e1eb0670a6aeb
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-07-31 10:02:27 +0300

    Checker receives selected norm in "check" method, not in constructor

--- station/src/norms/checker.py
+++ station/src/norms/checker.py
@@ -4,15 +4,11 @@ from norms.norm import Norm
 
 
 class NormChecker(QObject):
-    def __init__(self, norm: Norm):
-        super().__init__()
-        self._norm = norm
-
     @Signal
     def passed(self):
         pass
 
-    @Slot()
-    def check(self):
-        if self._norm.check():
+    @Slot(QObject)
+    def check(self, norm: Norm):
+        if norm.check():
             self.passed.emit()
