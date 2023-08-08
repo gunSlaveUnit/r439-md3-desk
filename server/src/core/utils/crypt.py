eed40a1184471c8397fa4164472105c412fad223
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-08-08 09:43:09 +0300

    verify_password func

--- server/src/core/utils/crypt.py
+++ server/src/core/utils/crypt.py
@@ -5,3 +5,7 @@ crypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_password_hash(password):
     return crypt_context.hash(password)
+
+
+def verify_password(plain_password, hashed_password):
+    return crypt_context.verify(plain_password, hashed_password)
